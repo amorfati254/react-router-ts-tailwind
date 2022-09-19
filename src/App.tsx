81c855500af755830debc380007b44cb7add3256
amorfati254 <amorfati254@gmail.com>
2022-09-19 11:16:10 +0300

    react-router-ts-tailwind

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+function App() {
+	const [count, setCount] = useState(0);
+
+	return (
+		<div className='App'>
+			<div className='text-4xl font-bold'>Hello</div>
+		</div>
+	);
+}
+
+export default App;
